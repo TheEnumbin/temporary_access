9b0362acc93148a0633a25625864a27a43f81ceb
TheEnumbin <enumbin@gmail.com>
2021-04-16 02:40:51 +0600

    Done adding new user in own table. Need to create actual user after add

--- classes/tempaccess.php
+++ classes/tempaccess.php
@@ -13,7 +13,7 @@ class tempaccess extends ObjectModel {
 
 
 	/**
-	 * Id_classyproductextratabs id of the item.
+	 * id_tempaccess id of the item.
 	 *
 	 * @var mixed
 	 */
@@ -73,47 +73,29 @@ class tempaccess extends ObjectModel {
 		'primary'   => 'id_tempaccess',
 		'multilang' => false,
 		'fields'    => array(
-			'content_type'          => array(
+			'name'                 => array(
 				'type'     => self::TYPE_STRING,
 				'validate' => 'isString',
+				'required' => true,
 			),
-			'modules_list'          => array(
+			'temp_email'                 => array(
 				'type'     => self::TYPE_STRING,
 				'validate' => 'isString',
+				'required' => true,
 			),
-			'module_hook_list'      => array(
+			'password'                 => array(
 				'type'     => self::TYPE_STRING,
 				'validate' => 'isString',
+				'required' => true,
 			),
-			'product_page'          => array(
-				'type'     => self::TYPE_STRING,
-				'validate' => 'isString',
-			),
-			'specific_product'      => array(
-				'type'     => self::TYPE_STRING,
-				'validate' => 'isString',
-			),
-			'specific_product_catg' => array(
-				'type'     => self::TYPE_STRING,
-				'validate' => 'isString',
-			),
-			'position'              => array( 'type' => self::TYPE_INT ),
+			'id_role'     			=> array( 'type' => self::TYPE_INT ),
+			'expire_date' => array('type' => self::TYPE_DATE, 'validate' => 'isDate'),
 			'active'                => array(
 				'type'     => self::TYPE_BOOL,
 				'validate' => 'isBool',
 				'required' => true,
 			),
-			'title'                 => array(
-				'type'     => self::TYPE_STRING,
-				'lang'     => true,
-				'validate' => 'isString',
-				'required' => true,
-			),
-			'content'               => array(
-				'type'     => self::TYPE_HTML,
-				'lang'     => true,
-				'validate' => 'isString',
-			),
+			'position'     			=> array( 'type' => self::TYPE_INT ),
 		),
 	);
 
@@ -127,7 +109,6 @@ class tempaccess extends ObjectModel {
 	 * @return void
 	 */
 	public function __construct( $id = null, $id_lang = null, $id_shop = null ) {
-		Shop::addTableAssociation( 'classyproductextratabs', array( 'type' => 'shop' ) );
 		parent::__construct( $id, $id_lang, $id_shop );
 	}
 
@@ -154,7 +135,7 @@ class tempaccess extends ObjectModel {
 	 */
 	public static function getHigherPosition() {
 		$sql      = 'SELECT MAX(`position`)
-                FROM `' . _DB_PREFIX_ . 'classyproductextratabs`';
+                FROM `' . _DB_PREFIX_ . 'tempaccess`';
 		$position = DB::getInstance()->getValue( $sql );
 		return ( is_numeric( $position ) ) ? $position : -1;
 	}
@@ -163,7 +144,7 @@ class tempaccess extends ObjectModel {
 	 * GetInstance provides the instance of the class.
 	 */
 	public static function GetInstance() {
-		$ins = new classyproductextratab();
+		$ins = new tempaccess();
 		return $ins;
 	}
 
@@ -176,133 +157,32 @@ class tempaccess extends ObjectModel {
 	public function updatePosition( $way, $position ) {
 		if ( ! $res = Db::getInstance()->executeS(
 			'
-            SELECT `id_classyproductextratabs`, `position`
-            FROM `' . _DB_PREFIX_ . 'classyproductextratabs`
+            SELECT `id_tempaccess`, `position`
+            FROM `' . _DB_PREFIX_ . 'tempaccess`
             ORDER BY `position` ASC'
 		)
 		) {
 			return false;
 		}
-		foreach ( $res as $classyproductextratabs ) {
-			if ( (int) $classyproductextratabs['id_classyproductextratabs'] == (int) $this->id ) {
-				$moved_classyproductextratabs = $classyproductextratabs;
+		foreach ( $res as $tempaccess ) {
+			if ( (int) $tempaccess['id_tempaccess'] == (int) $this->id ) {
+				$moved_tempaccess = $tempaccess;
 			}
 		}
-		if ( ! isset( $moved_classyproductextratabs ) || ! isset( $position ) ) {
+		if ( ! isset( $moved_tempaccess ) || ! isset( $position ) ) {
 			return false;
 		}
-		$query_1 = ' UPDATE `' . _DB_PREFIX_ . 'classyproductextratabs`
+		$query_1 = ' UPDATE `' . _DB_PREFIX_ . 'tempaccess`
         SET `position`= `position` ' . ( $way ? '- 1' : '+ 1' ) . '
         WHERE `position`
         ' . ( $way
-		? '> ' . (int) $moved_classyproductextratabs['position'] . ' AND `position` <= ' . (int) $position
-		: '< ' . (int) $moved_classyproductextratabs['position'] . ' AND `position` >= ' . (int) $position . '
+		? '> ' . (int) $moved_tempaccess['position'] . ' AND `position` <= ' . (int) $position
+		: '< ' . (int) $moved_tempaccess['position'] . ' AND `position` >= ' . (int) $position . '
         ' );
-		$query_2 = ' UPDATE `' . _DB_PREFIX_ . 'classyproductextratabs`
+		$query_2 = ' UPDATE `' . _DB_PREFIX_ . 'tempaccess`
         SET `position` = ' . (int) $position . '
-        WHERE `id_classyproductextratabs` = ' . (int) $moved_classyproductextratabs['id_classyproductextratabs'];
+        WHERE `id_tempaccess` = ' . (int) $moved_tempaccess['id_tempaccess'];
 		return ( Db::getInstance()->execute( $query_1 )
 		&& Db::getInstance()->execute( $query_2 ) );
-	}
-
-
-	/**
-	 * GetTabContentByProductId gets the tab contents by product id.
-	 *
-	 * @param mixed $id_product id of the product.
-	 */
-	public function GetTabContentByProductId( $id_product = 1 ) {
-		$reslt       = array();
-		$resltcat    = array();
-		$id_lang     = (int) Context::getContext()->language->id;
-		$id_shop     = (int) Context::getContext()->shop->id;
-		$sql         = 'SELECT * FROM `' . _DB_PREFIX_ . 'classyproductextratabs` v 
-                INNER JOIN `' . _DB_PREFIX_ . 'classyproductextratabs_lang` vl ON (v.`id_classyproductextratabs` = vl.`id_classyproductextratabs` AND vl.`id_lang` = ' . $id_lang . ')
-                INNER JOIN `' . _DB_PREFIX_ . 'classyproductextratabs_shop` vs ON (v.`id_classyproductextratabs` = vs.`id_classyproductextratabs` AND vs.`id_shop` = ' . $id_shop . ')
-                WHERE ';
-		$sql        .= ' v.`active` = 1 ORDER BY v.`position` ASC';
-		$sqlcat      = 'SELECT `id_category` FROM `' . _DB_PREFIX_ . 'category_product` 
-                		WHERE `id_product`=' . $id_product;
-		$cache_id    = md5( $sql );
-		$cachecat_id = md5( $sqlcat );
-
-		if ( ! Cache::isStored( $cache_id ) ) {
-			$resultcats = Db::getInstance()->executeS( $sqlcat );
-			if ( isset( $resultcats ) && ! empty( $resultcats ) ) {
-
-				foreach ( $resultcats as $i => $result ) {
-
-					$resltcat[] = $result['id_category'];
-				}
-			}
-		}
-
-		if ( ! Cache::isStored( $cache_id ) ) {
-			$results = Db::getInstance()->executeS( $sql );
-			if ( isset( $results ) && ! empty( $results ) ) {
-
-				foreach ( $results as $i => $result ) {
-					if ( isset( $result['product_page'] ) && $result['product_page'] == 1 ) {
-						$reslt[ $i ] = $result;
-					} elseif ( isset( $result['specific_product_catg'] ) && $result['specific_product_catg'] != '' ) {
-
-						$specific_product_catg_arr = explode( '-', $result['specific_product_catg'] );
-						unset( $specific_product_catg_arr[ count( $specific_product_catg_arr ) - 1 ] );
-						$intersect = array_intersect( $resltcat, $specific_product_catg_arr );
-						if ( isset( $intersect ) && ! empty( $intersect ) ) {
-							$reslt[ $i ] = $result;
-						}
-					} else {
-						$specific_product_arr = explode( '-', $result['specific_product'] );
-						if ( isset( $specific_product_arr ) && ! empty( $specific_product_arr ) ) {
-							unset( $specific_product_arr[ count( $specific_product_arr ) - 1 ] );
-							if ( in_array( $id_product, $specific_product_arr ) ) {
-								$reslt[ $i ] = $result;
-							}
-						}
-					}
-				}
-			}
-			$outputs = $this->ContentFilterEngine( $reslt );
-			Cache::store( $cache_id, $outputs );
-		}
-		return Cache::retrieve( $cache_id );
-	}
-
-	/**
-	 * ContentFilterEngine filters the results.
-	 *
-	 * @param mixed $results results fetched from database.
-	 */
-	public function ContentFilterEngine( $results = array() ) {
-		$outputs = array();
-		if ( isset( $results ) && ! empty( $results ) ) {
-			$i = 0;
-			foreach ( $results as $classypetab_values ) {
-				foreach ( $classypetab_values as $classypetab_key => $vcval ) {
-					if ( $classypetab_key == 'content' ) {
-						// $outputs[ $i ]['content'] = $this->vctab_content_filter( $vcval );
-						$outputs[ $i ]['content'] = $vcval;
-					}
-					if ( $classypetab_key == 'title' ) {
-						$outputs[ $i ]['title'] = $vcval;
-					}
-					if ( $classypetab_key == 'id_classyproductextratabs' ) {
-						$outputs[ $i ]['id_classyproductextratabs'] = $vcval;
-					}
-					if ( $classypetab_key == 'content_type' ) {
-						$outputs[ $i ]['content_type'] = $vcval;
-					}
-					if ( $classypetab_key == 'modules_list' ) {
-						$outputs[ $i ]['modules_list'] = $vcval;
-					}
-					if ( $classypetab_key == 'module_hook_list' ) {
-						$outputs[ $i ]['module_hook_list'] = $vcval;
-					}
-				}
-				$i++;
-			}
-		}
-		return $outputs;
 	}
 }
--- controllers/admin/AdminTempaccessController.php
+++ controllers/admin/AdminTempaccessController.php
@@ -19,6 +19,8 @@ class AdminTempaccessController extends ModuleAdminController{
 
 		parent::__construct();
 
+		include_once TEMPACCESS_CLASSES_PATH . 'tempaccess.php';
+
 		$this->fields_list = array(
 			'id_tempaccess' => array(
 				'title'   => $this->l( 'Id' ),
@@ -63,6 +65,132 @@ class AdminTempaccessController extends ModuleAdminController{
 
     }
 
+	/**
+	 * RenderList Renders the whole list.
+	 */
+	public function renderList() {
+		if ( isset( $this->_filter ) && trim( $this->_filter ) == '' ) {
+			$this->_filter = $this->original_filter;
+		}
+
+		$this->addRowAction( 'edit' );
+		$this->addRowAction( 'delete' );
+		return parent::renderList();
+	}
+
+	/**
+	 * RenderForm renders the create and edit form.
+	 *
+	 * @return void
+	 */
+	public function renderForm() {
+		$tempaccess_is_edit         = false;
+		
+		$edit_all                     = true;
+
+
+		$this->fields_form = array(
+			'legend'  => array(
+				'title' => $this->l( 'Add Temporary Access Account' ),
+			),
+			'input'   => array(
+				array(
+					'type'     => 'text',
+					'label'    => $this->l( 'Name' ),
+					'name'     => 'name',
+					'required' => true,
+					'desc'     => $this->l( 'Enter The Name' ),
+				),
+				array(
+					'type'     => 'text',
+					'label'    => $this->l( 'Email' ),
+					'name'     => 'temp_email',
+					'required' => true,
+					'desc'     => $this->l( 'Enter The Email' ),
+				),
+				array(
+					'type'     => 'text',
+					'label'    => $this->l( 'Password' ),
+					'name'     => 'password',
+					'required' => true,
+					'desc'     => $this->l( 'Enter The Email' ),
+				),
+				array(
+					'type'     => 'select',
+					'label'    => $this->l( 'Role' ),
+					'name'     => 'id_role',
+					'required' => true,
+					'options' => array(
+						'query' => array(
+                            array(
+								'id' => 'a',
+								'name' => 'A',
+							),
+							array(
+								'id' => 'b',
+								'name' => 'B',
+							),
+							array(
+								'id' => 'c',
+								'name' => 'C',
+							),
+                        ),
+                        'id' => 'id',
+                        'name' => 'name',
+                    ),
+					'desc'     => $this->l( 'Select Role for The User' ),
+				),
+				array(
+					'type'     => 'date',
+					'label'    => $this->l( 'Expire Date' ),
+					'name'     => 'expire_date',
+					'required' => true,
+				),
+				array(
+					'type'     => 'switch',
+					'label'    => $this->l( 'Status' ),
+					'name'     => 'active',
+					'required' => false,
+					'class'    => 't',
+					'is_bool'  => true,
+					'values'   => array(
+						array(
+							'id'    => 'active',
+							'value' => 1,
+							'label' => $this->l( 'Enabled' ),
+						),
+						array(
+							'id'    => 'active',
+							'value' => 0,
+							'label' => $this->l( 'Disabled' ),
+						),
+					),
+				),
+			),
+			'submit'  => array(
+				'title' => $this->l( 'Save And Close' ),
+				'class' => 'btn btn-default pull-right',
+			),
+			'buttons' => array(
+				'save-and-stay' => array(
+					'name'  => 'submitAdd' . $this->table . 'AndStay',
+					'type'  => 'submit',
+					'title' => $this->l( 'Save And Stay' ),
+					'class' => 'btn btn-default pull-right',
+					'icon'  => 'process-icon-save',
+				),
+			),
+		);
+		// if ( ! ( $classyproductextratabs = $this->loadObject( true ) ) ) {
+		// 	return;
+		// }
+		$this->fields_form['submit'] = array(
+			'title' => $this->l( 'Save And Close' ),
+			'class' => 'btn btn-default pull-right',
+		);
+
+		return parent::renderForm();
+	}
 
     public function initContent(){
         parent::initContent();
